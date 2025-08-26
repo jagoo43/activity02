85231ac0a069df3ac8673627abb0f025a80ed7f4
jagoo43 <jagoogamer1@gmail.com>
2025-08-26 18:19:57 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+print("hello world")
+age=15
+name="john"
+
+print(age)
+print(name)
+
+nameuser=input("please enter your name:")
+print(nameuser)
+
+age = int(input("enter your age:"))
+print(age)
